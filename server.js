37dbf72aa2f4b5cd37e834a4a7b9ffd6e623b7d4
sriramongit <sriramsahu187@gmail.com>
2025-08-19 10:16:01 +0530

    fixed port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cookieParser());
 
