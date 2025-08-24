4f5402cbd1e942e543bb441b3e30674fb719dc8c
sriramongit <sriramsahu187@gmail.com>
2025-08-24 12:07:14 +0530

    removed posts.js from gitignore

---
+++ models/posts.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }, 
+    content: String,
+    likes: [
+        {type: mongoose.Schema.Types.ObjectId, ref: "user"}
+    ]
+});
+
+module.exports = mongoose.model("Post", postSchema);
